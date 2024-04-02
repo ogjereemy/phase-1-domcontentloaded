5f8f1e38a0c1974bb4cf8b396d5738dabcbb28ea
ogola <ogolajeremy601@gmail.com>
2024-04-02 13:35:21 +0300

    adds-domContentLoaded-lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+// document.addEventListener("DOMContentLoaded", function(){
+//     console.log("This is really cool!");
+// })
+// console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
+document.addEventListener("DOMContentLoaded", function(){
+    //selects the element we want to modify
+    let texts = document.getElementById("text")
+    //changes  the text element 
+    texts.textContent = "This is really cool!";
+});
